91bc64b4b411d0e2c4c54f0669443ff60b2cae38
Vishwajeet Kumar <69458411+vish4030@users.noreply.github.com>
2024-07-14 14:15:15 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json())
 app.use(busRouter);
 app.use(userRouter);
+app.listen(process.env.PORT,()=>{console.log(`server working at port ${process.env.PORT}`)})
 
 
 
